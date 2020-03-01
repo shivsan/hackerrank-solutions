15faa98cb3cd53af5b2528c9d368305dc9cc1654
Shivku <sivakumar@finzy.com>
2020-03-01 21:34:10 +0530

    Two strings - optimized, sort of

--- src/main/java/com/hackerrank/TwoStrings.java
+++ src/main/java/com/hackerrank/TwoStrings.java
@@ -3,14 +3,9 @@ package com.hackerrank;
 public class TwoStrings {
     public String checkIfStringsHaveSubStrings(String s1, String s2) {
         boolean[] alphabets = new boolean[26];
-        for (char c : s1.toCharArray()) {
-            alphabets[c-97] = true;
-        }
 
-        for(char d: s2.toCharArray()){
-            if(alphabets[d-97]) return "YES";
-        }
+        s1.chars().forEach(s -> alphabets[s - 97] = true);
 
-        return "NO";
+        return s2.chars().filter(s -> alphabets[s - 97]).count() > 0 ? "YES" : "NO";
     }
 }
