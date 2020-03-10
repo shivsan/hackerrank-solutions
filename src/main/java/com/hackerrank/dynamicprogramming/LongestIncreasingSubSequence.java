f92bce62771a7c53ff5441e53c5c01f191e419df
Shivku <sivakumar@finzy.com>
2020-03-10 10:03:11 +0530

    Longest increasing subsequence

---
+++ src/main/java/com/hackerrank/dynamicprogramming/LongestIncreasingSubSequence.java
@@ -0,0 +1,24 @@
+package com.hackerrank.dynamicprogramming;
+
+import java.util.stream.IntStream;
+
+public class LongestIncreasingSubSequence {
+    public int[] max;
+
+    public int get(int[] sequence) {
+        max = new int[sequence.length];
+        max[0] = 1;
+        return findMax(sequence, sequence.length - 1);
+    }
+
+    private int findMax(int[] arr, int i) {
+        if (max[i] != 0)
+            return max[i];
+
+        return max[i] = 1 + maxOfArray(IntStream.range(0, i).filter(k -> arr[k] < arr[i]).map(k -> findMax(arr, k)));
+    }
+
+    private int maxOfArray(IntStream array) {
+        return array.max().orElse(0);
+    }
+}
---
+++ src/test/java/com/hackerrank/dynamicprogramming/LongestIncreasingSubSequenceTest.java
@@ -0,0 +1,30 @@
+package com.hackerrank.dynamicprogramming;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(JUnit4.class)
+public class LongestIncreasingSubSequenceTest {
+
+    public LongestIncreasingSubSequence longestIncreasingSubSequence;
+
+    @Before
+    public void init() {
+        longestIncreasingSubSequence = new LongestIncreasingSubSequence();
+    }
+
+    @Test
+    public void getLongestIncreasingSubSequenceSize() {
+        final var sequence = new int[]{10, 22, 11, 12, 40, 13, 14, 50};
+
+        final var longestIncreasingSubSequenceSize = this.longestIncreasingSubSequence.get(sequence);
+
+        assertEquals(longestIncreasingSubSequenceSize, 6);
+    }
+}
