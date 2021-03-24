f70e66df6af96e3e87ca951e3f9af80ae9346149
Siva Kumar <shivku.goku@gmail.com>
2021-03-24 23:04:38 +0530

    Queue using stack

---
+++ src/main/java/com/hackerrank/QueueUsingStack.java
@@ -0,0 +1,10 @@
+package com.hackerrank;
+
+public class QueueUsingStack {
+    public static class Solution {
+
+        public static void main(String[] args) {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        }
+    }
+}
