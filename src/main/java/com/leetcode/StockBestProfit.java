abf3c1d758bc368a6b1cd642b6a097884224a502
Siva Kumar <shivku.goku@gmail.com>
2021-10-17 11:27:47 +0530

    Most profit with stocks - DP.

--- src/main/java/com/leetcode/StockBestProfit.java
+++ src/main/java/com/leetcode/StockBestProfit.java
@@ -53,4 +53,21 @@ public class StockBestProfit {
                 .findFirst()
                 .get();
     }
+
+    public int getBestProfitDP(int[] stockPrices) {
+        int boughtState[] = new int[stockPrices.length];
+        int soldState[] = new int[stockPrices.length];
+        int cooledState[] = new int[stockPrices.length];
+        boughtState[0] = -stockPrices[0];
+        soldState[0] = 0;
+        cooledState[0] = 0;
+
+        for (int i = 1; i < stockPrices.length; i++) {
+            boughtState[i] = Integer.max(boughtState[i - 1], cooledState[i - 1] - stockPrices[i]);
+            soldState[i] = boughtState[i - 1] += stockPrices[i];
+            cooledState[i] = Integer.max(cooledState[i - 1], soldState[i - 1]);
+        }
+
+        return Integer.max(soldState[soldState.length - 1], cooledState[cooledState.length - 1]);
+    }
 }
--- src/test/java/com/leetcode/StockBestProfitTest.java
+++ src/test/java/com/leetcode/StockBestProfitTest.java
@@ -5,17 +5,47 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class StockBestProfitTest {
+    StockBestProfit stockBestProfit = new StockBestProfit();
+
     @Test
     public void shouldGetBestPrice() {
-        int bestProfit = new StockBestProfit().getBestProfit(new int[]{1, 2, 3, 0, 2});
+        int bestProfit = stockBestProfit.getBestProfit(new int[]{1, 2, 3, 0, 2});
 
         assertEquals(3, bestProfit);
     }
 
     @Test
     public void shouldGetBestPrice1() {
-        int bestProfit = new StockBestProfit().getBestProfit(new int[]{1});
+        int bestProfit = stockBestProfit.getBestProfit(new int[]{1});
 
         assertEquals(0, bestProfit);
+    }
+
+    @Test
+    public void shouldGetBestPrice2DP() {
+        int bestProfit = stockBestProfit.getBestProfitDP(new int[]{48, 12, 60, 93, 97, 42, 25, 64, 17, 56, 85, 93, 9, 48, 52, 42, 58, 85, 81, 84, 69, 36, 1, 54, 23, 15, 72, 15, 11, 94});
+
+        assertEquals(428, bestProfit);
+    }
+
+    @Test
+    public void shouldGetBestPriceDP() {
+        int bestProfit = stockBestProfit.getBestProfitDP(new int[]{1, 2, 3, 0, 2});
+
+        assertEquals(3, bestProfit);
+    }
+
+    @Test
+    public void shouldGetBestPrice1DP() {
+        int bestProfit = stockBestProfit.getBestProfitDP(new int[]{1});
+
+        assertEquals(0, bestProfit);
+    }
+
+    @Test
+    public void shouldGetBestPriceDP3() {
+        int bestProfit = stockBestProfit.getBestProfitDP(new int[]{2, 1, 4});
+
+        assertEquals(3, bestProfit);
     }
 }
